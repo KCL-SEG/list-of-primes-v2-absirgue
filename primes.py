7df24e1428d21e341f8e9e2fd0659c41e0673f62
absirgue <anton.sirgue@kcl.ac.uk>
2022-09-26 18:40:18 +0100

    Add negative number error raising
    Build on previous version of the project to raise an exception when negative parameters are given.

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("number of primes should be positive")
     list = []
+    tested_number = 1
+    while len(list) < number_of_primes:
+        numberCanBePrime = True
+        tested_number += 1
+        for i in range(2, tested_number):
+            if (tested_number % i == 0):
+                numberCanBePrime = False
+        if (numberCanBePrime):
+            list.append(tested_number)
     return list
